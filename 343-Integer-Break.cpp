3441323265d76ee4294fc9f785aa05eacde66aca
AC Violet <30395886+m13953842591@users.noreply.github.com>
2019-03-10 15:22:15 +0800

    update

---
+++ 343-Integer-Break.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int integerBreak(int n) {
+    if(n == 2) return 1;
+    if(n == 3) return 2;
+    int product = 1;
+    while(n > 4){
+        product *= 3;
+        n -= 3;
+    }
+    product *=n;
+    return product;
+} 
+
+int main(){
+    cout << integerBreak(10) << endl;
+    return 0;
+}
