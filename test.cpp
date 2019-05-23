d78d3ef91e52e79bcfc85c81950be8b55741bd3b
AC Violet <30395886+m13953842591@users.noreply.github.com>
2019-05-23 08:18:23 +0800

    update

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int main(){
+    vector<int> a = vector<int>({1,2});
+    for(int i = 0; i < 2; i++)
+    {
+        cout << a[i] << endl;
+    }
+    return 0;
+}
